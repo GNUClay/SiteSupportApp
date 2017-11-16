c161ab5604ed04e014a5522cc99da3e39561e820
Sergiy Tolkachov <metatypeman@gmail.com>
2017-11-16 17:52:09 +0200

    #4

--- SiteSupportApp/SiteGenerator/ApiReferenceGenerator/ClassInfo.cs
+++ SiteSupportApp/SiteGenerator/ApiReferenceGenerator/ClassInfo.cs
@@ -19,13 +19,31 @@ namespace SiteGenerator.ApiReferenceGenerator
         private XMLDocWrapper mDoc;
         public string FullName { get; private set; }
 
+        public List<string> Properties { get; private set; } = new List<string>();
+        public List<string> Methods { get; private set; } = new List<string>();
+        public List<string> Events { get; private set; } = new List<string>();
+
         private void FillMembers()
         {
             var propertiesList = mDoc.GetPropertiesNames(FullName);
 
             foreach(var item in propertiesList)
             {
-                NLog.LogManager.GetCurrentClassLogger().Info($"GetPropertiesNames item = {item }");
+                Properties.Add(item);
+            }
+
+            var methodsList = mDoc.GetMethodsNames(FullName);
+
+            foreach (var item in methodsList)
+            {
+                NLog.LogManager.GetCurrentClassLogger().Info($"FillMembers Method item = {item}");
+            }
+
+            var eventsList = mDoc.GetEventsNames(FullName);
+
+            foreach (var item in eventsList)
+            {
+                NLog.LogManager.GetCurrentClassLogger().Info($"FillMembers Events item = {item}");
             }
         }
     }
